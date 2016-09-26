17927146a7d293a49fb1d63bbe38df372dffc160
bindrigodossantos <bindrigodossantos@presta.aldebaran.com>
2016-09-26 09:12:33 +0200

    update soundLoc

--- test_soundlocalization.py
+++ test_soundlocalization.py
@@ -1,6 +1,7 @@
 import math
 import qi
 import sys
+import time
 
 __author__ = "bindrigodossantos"
 __copyright__ = "Copyright June 2016, Aldebaran Robotics"
@@ -8,7 +9,7 @@ __copyright__ = "Copyright June 2016, Aldebaran Robotics"
 """
 Look at the sound source, using the FRAME_WORLD, with a speed fraction 0.1 and using only the head 
 """
-
+print sys.argv[0], sys.argv[1]
 session = qi.Session()
 session.connect(sys.argv[1])
 
@@ -18,22 +19,29 @@ memory = session.service("ALMemory")
 soundloc = session.service("ALSoundLocalization")
 life = session.service("ALAutonomousLife")
 
-life.setState("disabled")
+#life.setState("disabled")
 motion.wakeUp()
 
-soundloc.subscribe("toto")
+soundloc.subscribe("SoundLocate")
 print "Type CTRL+C to stop soundLocalization"
 
 try:
     while(True):
         soundPos = memory.getData("ALSoundLocalization/SoundLocated")[1]
-	
-	x = math.cos(soundPos[1])*math.cos(soundPos[0])
-	y = math.cos(soundPos[1])*math.sin(soundPos[0])
-	z = math.sin(-soundPos[1])		# elevation doesn't work very well
-	
-	tracker.lookAt([x,y,0],0,0.1,0)
+        print "polar coord = ", soundPos, "\n"
+        print "confidence = ", soundPos[2], "\n"
+        
+        if soundPos[2] > 0.75:
+            x = math.cos(soundPos[1])*math.cos(soundPos[0])
+            y = math.cos(soundPos[1])*math.sin(soundPos[0])
+            z = math.sin(-soundPos[1])		# elevation doesn't work very well
+            print "x,y,z = ", [x,y,z], "\n"
+
+            tracker.lookAt([x,y,z],0,0.1,0)
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
+    # unsubscribe and stop sound localization
+    soundloc.unsubscribe("SoundLocate")
     print "Test Interrupted!"
 
